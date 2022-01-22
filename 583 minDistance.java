8b68080d2ed683b937c01615259c647c2818ce7a
OrpheusLin <OliveStar@users.noreply.github.com>
2022-01-22 22:44:57 +0800

    Add files via upload
    leetcode 583. 两个字符串的删除操作 带备忘录的递归

---
+++ 583 minDistance.java
@@ -0,0 +1,29 @@
+class Solution {
+    int[][] memo;
+    String w1;
+    String w2;
+    public int minDistance(String word1, String word2) {
+        memo = new int[word1.length()][word2.length()];
+        for(int i=0; i < word1.length(); i++){
+            Arrays.fill(memo[i], Integer.MAX_VALUE);
+        }
+        w1 = word1;
+        w2 = word2;
+        return dp(w1.length()-1, w2.length()-1);
+    }
+
+    int dp(int i,int j){
+        // base case：s1或s2有1个先遍历结束时，直接把剩下的一个串全删了
+        // i,j是下标，实际删除的是长度，比下标多1
+        if(i == -1) return j+1;
+        if(j == -1) return i+1;
+        if(memo[i][j] != Integer.MAX_VALUE)
+            return memo[i][j];
+        if(w1.charAt(i) == w2.charAt(j)){
+            memo[i][j] = dp(i-1, j-1); // 如果当前遍历的两个字符相等，不需要删除，继续往前搜索
+        }else{
+            memo[i][j] = Math.min(dp(i-1,j)+1, dp(i,j-1)+1); // 当前遍历的两个字符不相等，i或j各往前搜索1步，选择最小的
+        }
+        return memo[i][j];
+    }
+}
