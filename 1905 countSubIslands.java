2864dd3e1e534906280622b88510cdfcc644871a
OrpheusLin <OliveStar@users.noreply.github.com>
2022-04-07 20:10:15 +0800

    Add files via upload
    统计子岛屿数目，DFS，FloodFill算法

---
+++ 1905 countSubIslands.java
@@ -0,0 +1,42 @@
+class Solution {
+    // 和封闭岛屿的题目类似，那题排除靠边的岛屿，本题排除不可能是子岛的岛屿
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        int m = grid1.length, n = grid1[0].length;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid1[i][j] == 0 && grid2[i][j] == 1){
+                    // 这个岛屿肯定不是子岛，淹掉
+                    dfs(grid2, i, j);
+                }
+            }
+        }
+        // 现在grid2中剩下的岛屿都是子岛，计算岛屿数量
+        int res = 0;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid2[i][j] == 1){
+                    res++;
+                    dfs(grid2, i, j);
+                }
+            }
+        }
+        return res;
+    }
+
+    // 从（i,j）开始，将与之相邻的陆地都变成海水
+    void dfs(int[][] grid, int i, int j){
+        int m = grid.length, n = grid[0].length;
+        if(i < 0 || j < 0 || i >= m || j >= n){
+            return;
+        }
+        if(grid[i][j] == 0){
+            return;
+        }
+
+        grid[i][j] = 0;
+        dfs(grid, i+1, j);
+        dfs(grid, i, j+1);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j-1);
+    }
+}
