8c78b6d27dd527603affefefbfa78bf4058f6f69
OrpheusLin <OliveStar@users.noreply.github.com>
2022-05-15 12:34:54 +0800

    Add files via upload
    大数加法，字符串相加

---
+++ 415 addStrings.java
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+        算法：大数相加，模拟竖式计算，从最后一位开始往前逐位相加
+        先到头的，后面就都是0。
+     */
+    public String addStrings(String num1, String num2) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0; // 记录进位的变量
+        for(int i = num1.length()-1, j = num2.length() - 1; i >= 0 || j >= 0 || carry == 1; i--, j--){
+            int x = i < 0? 0 : num1.charAt(i) - '0';
+            int y = j < 0? 0 : num2.charAt(j) - '0';
+            sb.append((x + y + carry) % 10);
+            carry = (x + y + carry) / 10;
+        }
+        return sb.reverse().toString();
+    }
+}
