45881e4bf66528fc2ade25a9b7ee1be6a05ddf0b
OrpheusLin <OliveStar@users.noreply.github.com>
2022-06-16 23:05:02 +0800

    Add files via upload
    动态规划

---
+++ 63 uniquePathsWithObstacles.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    /**
+    * 算法：和62题没太大差别，在遇到障碍时，直接将障碍位置到右下角的路径数置为0即可
+    **/
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n = obstacleGrid.size(), m = obstacleGrid.at(0).size();
+        vector <int> f(m);
+
+        f[0] = (obstacleGrid[0][0] == 0);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (obstacleGrid[i][j] == 1) {
+                    f[j] = 0;
+                    continue;
+                }
+                if (j - 1 >= 0 && obstacleGrid[i][j - 1] == 0) {
+                    f[j] += f[j - 1];
+                }
+            }
+        }
+
+        return f.back();
+    }
+};
